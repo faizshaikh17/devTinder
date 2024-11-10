a712ab2c2786e53006938bb2f933fe9460fd4fce
faizshaikh17 <mohdfaiz8101@gmail.com>
2024-11-10 11:51:16 +0530

    added feed API with pagination

--- src/routers/user.js
+++ src/routers/user.js
@@ -3,6 +3,7 @@ const { userAuth } = require('../middlewares/auth');
 const { Connection } = require('mongoose');
 const userRouter = express.Router();
 const ConnectionRequest = require("../models/connectionRequest");
+const User = require("../models/user")
 
 userRouter.get("/user/request/recieved", userAuth, async (req, res) => {
     try {
@@ -40,9 +41,6 @@ userRouter.get("/user/connection", userAuth, async (req, res) => {
 
         }).populate("fromUserId", "firstName lastName").populate("toUserId", "firstName lastName")
 
-        // console.log(connectionRequest.fromUserId);
-        // console.log(loggedInUser.firstName);
-
         if (connectionRequest == "") {
             throw new Error("No user found");
         }
@@ -55,7 +53,6 @@ userRouter.get("/user/connection", userAuth, async (req, res) => {
                 return row.fromUserId.firstName;
             }
         })
-        // console.log();
 
         res.json({
             message: "Your connection data",
@@ -63,6 +60,44 @@ userRouter.get("/user/connection", userAuth, async (req, res) => {
         })
     } catch (error) {
         res.status(400).send("No connection found" + error.message)
+    }
+})
+
+userRouter.get("/user/feed", userAuth, async (req, res) => {
+    try {
+        const loggedInUser = req.user;
+        const page = req.query.page;
+        let limit = req.query.limit;
+        limit = limit > 50 ? 50 : limit;
+
+        const connectionRequest = await ConnectionRequest.find({
+            $or: [{ fromUserId: loggedInUser._id }, { toUserId: loggedInUser._id }]
+
+        }).select("fromUserId toUserId");
+
+        const feedBlockedUser = new Set();
+        connectionRequest.forEach(req => {
+            feedBlockedUser.add(req.fromUserId.toString());
+            feedBlockedUser.add(req.toUserId.toString());
+        });
+
+        const user = await User.find({
+            $and: [
+                { _id: { $ne: loggedInUser._id } },
+                { _id: { $nin: Array.from(feedBlockedUser) } }
+            ]
+        }).select("firstName lastName")
+            .skip((page - 1) * limit)
+            .limit(limit);
+
+        // console.log(connectionRequest);
+        res.json({
+            message: "your feed",
+            user
+        })
+
+    } catch (error) {
+        res.status(400).send("feed not found " + error.message);
     }
 })
 
